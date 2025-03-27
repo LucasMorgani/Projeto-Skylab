7f949a39fe543e8e3e86eb79a9660fe72ea0e9d2
André Abdala <andreluizabdalacosta@gmail.com>
2025-03-27 13:29:55 -0300

    Fiz uma interface simples

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Hello, Flask!"
+    return render_template("homepage.html")
 
 @app.route("/teste")
 def users():
@@ -12,4 +12,3 @@ def users():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
